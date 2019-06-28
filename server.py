7a069fe76570a4cce3c91e29a1d052128b154831
Roger Iyengar <ri@rogeriyengar.com>
2019-06-28 16:29:47 -0400

    Got server upload working

--- server.py
+++ server.py
@@ -1,8 +1,26 @@
 import socket
+import struct
 
 
 HOST=''
 PORT=8089
+
+
+def recv_n_bytes(conn, n):
+    """ Convenience method for receiving exactly n bytes from
+        socket (assuming it's open and connected).
+    """
+
+    # based on https://docs.python.org/3.4/howto/sockets.html
+    chunks = []
+    bytes_recd = 0
+    while bytes_recd < n:
+        chunk = conn.recv(n - bytes_recd)
+        if chunk == b'':
+            raise RuntimeError("socket connection broken")
+        chunks.append(chunk)
+        bytes_recd += len(chunk)
+    return b''.join(chunks)
 
 
 def main():
@@ -11,6 +29,7 @@ def main():
     s.listen(10)
 
     conn, addr=s.accept()
+    print('Device connected')
     data_size = struct.unpack("!I", recv_n_bytes(conn, 4))[0]
 
     with open('video.mp4', 'wb') as video_file:
